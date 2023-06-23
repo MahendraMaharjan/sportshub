4d3ec592e9de39868ef9596fa79d54850f711f7b
MahendraMaharjan <71314293+MahendraMaharjan@users.noreply.github.com>
2023-06-23 09:32:43 +0545

    Add files via upload

---
+++ for-admin-pages/index.php
@@ -0,0 +1,9 @@
+<?php include './includes/header.php'; ?>
+<?php include './includes/navbar.php'; ?>
+<div id="mainPart">
+
+    <?php
+    include './options.php';
+    ?>
+</div>
+<?php include './includes/footer.php'?>
